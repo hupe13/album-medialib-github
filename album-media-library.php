e270cefdbd430aebe7d810fa628c77192cb895f7
hupe13 <hupe13@t-online.de>
2025-05-26 09:34:47 +0200

    test

---
+++ album-media-library.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Version:           250509
+ * @package album-medialib
+ **/
+
+// Direktzugriff auf diese Datei verhindern.
+defined( 'ABSPATH' ) || die();
+
+include_once ALBUM_MEDIALIB_DIR . 'album-medialib.php';
